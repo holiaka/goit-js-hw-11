b5c14226f06572aa5a4f5892346cb68c7b2a0963
Dmytrii Holiaka <holyaka_d_m@ukr.net>
2022-11-29 23:59:34 +0200

    Control_3

--- src/index.js
+++ src/index.js
@@ -1,8 +1,43 @@
 // Libries import
 import './css/styles.css';
-import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
+import { fetchPhoto } from './js/fetchFunctions';
 
-// Key var.
-const DEBOUNCE_DELAY = 300;
+const serchRequest = document.querySelector('.search-form');
+serchRequest.addEventListener('submit', onSubmitRequest);
 
+function onSubmitRequest(evt) {
+  evt.preventDefault();
+  const submitText = evt.currentTarget.elements.searchQuery.value;
+  console.log(submitText);
+  fetchPhoto(submitText, 1).then(photos => createHtmlPhotosColection(photos.data.hits));
+}
+
+let scrollOptions = {
+  root: null,
+  rootMargin: '300px',
+  threshold: 1.0,
+};
+
+// let observer = new IntersectionObserver(callback, scrollOptions);
+
+// const targetForScroll = document.querySelector('.js-target')
+// observer.observe(targetForScroll);
+
+const photoGallary = document.querySelector('.gallery');
+
+function createHtmlPhotosColection(photosData) {
+    const photosList = photosData.map((item) => createOneCard(item));
+    photoGallary.insertAdjacentHTML('beforeend', photosList.join())
+}
+
+function createOneCard(item) {
+    const {webformatURL, largeImageURL, tags, likes, views, comments, downloads} = item
+    return oneCard = 
+        `<a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy/>            
+        <div class="info"><p class="info-item"><b>Likes</b><span>${likes}</span></p>
+                <p class="info-item"><b>Views</b><span>${views}</span></p>
+                <p class="info-item"><b>Comments</b><span>${comments}</span></p>
+                <p class="info-item"><b>Downloads</b><span>${downloads}</span></p>
+        </div></a>`;
+}
--- src/js/fetchFunctions.js
+++ src/js/fetchFunctions.js
@@ -1,11 +1,23 @@
+import axios from 'axios';
+
+const API_KEY = '30951903-ffa881e7e59a7b1cacd7ea887';
+
 // Fetch func.
-export async function fetchCountries(name) {
-  const response = await fetch(
-    `https://pixabay.com/api/?key=30951903-ffa881e7e59a7b1cacd7ea887&q=yellow+flowers&image_type=photo`
-  );
-  if (!response.ok) {
-    throw new Error(response.status);
+export async function fetchPhoto(serchRequest, photoColection) {
+  try {
+    const response = await axios.get('https://pixabay.com/api/', {
+      params: {
+        key: `${API_KEY}`,
+        q: `${serchRequest}`,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 40,
+        page: `${photoColection}`,
+      },
+    });
+    return response;    
+  } catch (error) {
+    console.error(error);
   }
-  console.dir(response);
-  return await response.json();
 }
